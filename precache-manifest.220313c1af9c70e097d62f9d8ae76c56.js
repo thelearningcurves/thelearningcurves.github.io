171ceaba4c378ef2dee8153f73ed547b5712e429
iminfinity <this.is.me.infinity@gmail.com>
2020-10-04 18:28:00 +0530

    Updates

---
+++ precache-manifest.220313c1af9c70e097d62f9d8ae76c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad20f4eb901b9f938ae5b9dd3c6dbe90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e8d98917dd49c21fb94",
+    "url": "/static/css/main.700e62c4.chunk.css"
+  },
+  {
+    "revision": "c770cb4cb8408412d984",
+    "url": "/static/js/2.8c8de03e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c8de03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8d98917dd49c21fb94",
+    "url": "/static/js/main.fd06d056.chunk.js"
+  },
+  {
+    "revision": "15935c998ef8e9021b8a",
+    "url": "/static/js/runtime-main.dd449fdf.js"
+  }
+]);
