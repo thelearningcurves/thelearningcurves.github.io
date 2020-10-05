3b483db4ffbfdf8638645d147fa7304d3d71264e
iminfinity <this.is.me.infinity@gmail.com>
2020-10-05 11:27:38 +0530

    Updates

---
+++ precache-manifest.241ee70473b132e1cf0324438ce722f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d545a6e53c793a670504971f2bf836d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e73399782872741318",
+    "url": "/static/css/main.e8dc48f9.chunk.css"
+  },
+  {
+    "revision": "cd079eeb6d4cec8413be",
+    "url": "/static/js/2.dfd898a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfd898a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e73399782872741318",
+    "url": "/static/js/main.04e0d49f.chunk.js"
+  },
+  {
+    "revision": "15935c998ef8e9021b8a",
+    "url": "/static/js/runtime-main.dd449fdf.js"
+  },
+  {
+    "revision": "51795592f4adff7a1b834b6d262843e7",
+    "url": "/static/media/instagram.51795592.svg"
+  },
+  {
+    "revision": "f3a7756af301c0ec22eae534bf088a6f",
+    "url": "/static/media/kaggle.f3a7756a.svg"
+  },
+  {
+    "revision": "e7ccc098fdee284ae94ea7acda096f3a",
+    "url": "/static/media/linkedin.e7ccc098.svg"
+  },
+  {
+    "revision": "c97a6c3c86213f93a7ce17a67db005a1",
+    "url": "/static/media/menu.c97a6c3c.svg"
+  },
+  {
+    "revision": "c40bca7ecd9f9cf246b27deda3482344",
+    "url": "/static/media/profile-pic-temp.c40bca7e.svg"
+  },
+  {
+    "revision": "99489de29a5b076e4dad5df4b1b3760d",
+    "url": "/static/media/twitter.99489de2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed092a05e6858794fa569f2849aeae9f.js"
+  "/precache-manifest.241ee70473b132e1cf0324438ce722f8.js"
 );
 
 self.addEventListener('message', (event) => {
