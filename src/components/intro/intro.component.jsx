720abbadbf22a8988189b3f9625ff26e1489cb2a
geophysicsrtd <51423544+geophysicsrtd@users.noreply.github.com>
2020-10-05 20:29:07 +0545

    added hyperlinks

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import "./intro.styles.scss";
 
+import { ReactComponent as ProfilePic } from "../../assets/profile-pic-temp.svg";
 const Intro = () => {
   return (
     <div className="intro">
-      <div className="profile-pic" />
+      <ProfilePic />
       <div>
         <p>
-<<<<<<< HEAD
-          Hello!
-          My name is Shobhit Mishra.
-          I am a data-driven geophysicist focused on implementing Machine Learning
+          Hello! I am Shobhit,
+           a data-driven geophysicist focused on implementing Machine Learning
           to solve the inverse geophysical problems. 
           
           
-=======
-          Hello! I am a data-driven geophysicist focused on implementing Machine
-          Learning to solve the inverse geophysical problems.
->>>>>>> 91a027908d5de78a7db8491a0cbc4b7c8c975cc2
         </p>
         <p>
-          When I am not thinking about ML, I like to read novels, nowadays
-          mostly by russian novelist or re-reading a Murakami.
+          When I am not thinking about ML, I like to read novels, nowadays mostly by
+          russian novelist or re-reading a Murakami.
         </p>
         <p>
           I am looking for cool projects that images earth's sub-surface. I am
-          always willing to learn about new domain and often take it as a
-          challenge.
+          always willing to learn about new domain and often take it as a challenge.
+          
         </p>
       </div>
     </div>
